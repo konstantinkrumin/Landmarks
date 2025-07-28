6b4f133539447cbbe14e3d1079ed9b594186a889
Konstantin Krumin <konstantin.krumin@intellitix.com>
2025-07-28 19:10:25 -0400

    Create an icon

---
+++ Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Konstantin Krumin on 2025-07-28.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
